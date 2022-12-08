aa8ba99af0fbf5b542ecb2660ef56e561915afa1
Evgeniy Arkov <arhasik1905@mail.ru>
2022-12-09 00:24:30 +0500

    screen

---
+++ app/src/main/java/com/example/screenlayout/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.screenlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.screenlayout.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.custom
+    }
+}
